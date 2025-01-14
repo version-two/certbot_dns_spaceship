9bd8134df129b78fcd675aa2f1ea4ebe837c9ae5
Mario Chamuty <chamuty@versiontwo.sk>
2025-01-14 03:01:01 +0100

    Structure changes

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name="certbot-dns-spaceship",
-    version="0.1.0",
+    version="1.0.0",
     description="Spaceship DNS Authenticator plugin for Certbot",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/your-username/certbot-dns-spaceship",
-    author="Your Name",
-    author_email="your-email@example.com",
+    url="https://github.com/version-two/certbot_dns_spaceship",
+    author="Mario Chamuty",
+    author_email="info@versiontwo.sk",
     license="Apache License 2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "certbot>=1.0.0",
         "requests",
+        "tldextract",
     ],
     entry_points={
         "certbot.plugins": [
@@ -23,11 +23,9 @@ setup(
         ],
     },
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Internet :: WWW/HTTP",
-        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
     ],
 )
     
